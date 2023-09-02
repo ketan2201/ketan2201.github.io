58be5d077a73543d5fe161db5a61bb87b4837774
ketan <sakpalketank@gmail.com>
2023-09-02 16:36:38 +0530

    Update Summary

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const filterButtons = document.querySelectorAll(".filter-button");
+    const products = document.querySelectorAll(".product");
+
+    filterButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const filterValue = button.getAttribute("data-filter");
+            
+            products.forEach(product => {
+                const productType = product.getAttribute("data-type");
+
+                if (filterValue === "all" ||
+                    (filterValue === "new" && productType === "new") ||
+                    (filterValue === "best-seller" && productType === "best-seller")) {
+                    product.style.display = "block";
+                } else {
+                    product.style.display = "none";
+                }
+            });
+        });
+    });
+});
 
